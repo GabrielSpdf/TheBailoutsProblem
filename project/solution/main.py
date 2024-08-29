617631321a6b0a23fce570b0bc7615eecb68abf0
GabrielSpdf <gb.spada@hotmail.com>
2024-08-29 16:48:54 -0300

    reestruturacao de pastas

---
+++ project/solution/main.py
@@ -0,0 +1,57 @@
+import sys
+import os
+import time
+
+## importa classes
+from vs.environment import Env
+from explorer import Explorer
+from rescuer import Rescuer
+from manager import Manager
+
+def main(data_folder_name):
+   
+    # Set the path to config files and data files for the environment
+    current_folder = os.path.abspath(os.getcwd())
+    data_folder = os.path.abspath(os.path.join(current_folder, data_folder_name))
+
+    
+    # Instantiate the environment
+    env = Env(data_folder)
+    
+    # config files for the agents
+    rescuer_file = os.path.join(data_folder, "rescuer_config.txt")
+    explorer_file = os.path.join(data_folder, "explorer_config.txt")
+    
+    # Instantiate agents rescuer and explorer
+    resc1 = Rescuer(env, rescuer_file, 1)
+    resc2 = Rescuer(env, rescuer_file, 2)
+    resc3 = Rescuer(env, rescuer_file, 3)
+    resc4 = Rescuer(env, rescuer_file, 4)
+
+    man = Manager(resc1, resc2, resc3, resc4)
+
+    #Vetor de prioridade de movimentos
+    v1 = [2, 1, 0, 7, 6, 5, 4, 3]
+    v2 = [2, 3, 4, 5, 6, 7, 0, 1]
+    v3 = [6, 7, 0, 1, 2, 3, 4, 5]
+    v4 = [6, 5, 4, 3, 2, 1, 0, 7]
+
+    exp1 = Explorer(env, explorer_file, man, v1)
+    exp2 = Explorer(env, explorer_file, man, v2)
+    exp3 = Explorer(env, explorer_file, man, v3)
+    exp4 = Explorer(env, explorer_file, man, v4)
+
+    # Run the environment simulator
+    env.run()
+    
+        
+if __name__ == '__main__':
+    """ To get data from a different folder than the default called data
+    pass it by the argument line"""
+    
+    if len(sys.argv) > 1:
+        data_folder_name = sys.argv[1]
+    else:
+                    data_folder_name = os.path.join("datasets", "data_42v_20x20")
+        
+    main(data_folder_name)
---
+++ project/solution/manager.py
@@ -0,0 +1,87 @@
+import numpy as np
+import xgboost as xgb
+import joblib
+from map import Map
+from cluster import *
+
+class Manager():
+    def __init__(self, resc1, resc2, resc3, resc4):
+        # Utilizar classificador 
+        self.model = xgb.XGBClassifier()  
+        self.model.load_model('/home/gaspad/prog/SistemasInteligentes/project/solution/estimate/modelo_xgboost.json')
+        
+        # Utilizar MLP
+        # self.model = joblib.load('/home/gaspad/prog/SistemasInteligentes/project/solution/estimate/modelo_mlp.pkl')
+
+        self.resc1 = resc1
+        self.resc2 = resc2
+        self.resc3 = resc3
+        self.resc4 = resc4
+        self.map = Map()             
+        self.victims = {}         
+        self.map_counter = 0        
+
+    # Espera-se que haja quatro mapas
+    def full_join_maps(self, map):
+        print(f"Resgate com {self.map_counter} mapa(s)")
+        if(self.map_counter == 0):
+            self.map = map
+            self.map_counter = self.map_counter + 1
+
+        elif(self.map_counter == 1 or self.map_counter == 2):
+            self.map.union_maps(map)
+            self.map_counter = self.map_counter + 1
+
+        elif(self.map_counter == 3):
+            self.map.union_maps(map)
+            self.map_counter = self.map_counter + 1
+            print("Resgate com todos os mapas!")
+            self.plan_to_rescuer()
+            print("Clusterizacao concluida!")
+
+
+    def assign_victims_to_rescuers(self, centroids):
+        """Atribui as vítimas de cada cluster aos resgatadores."""
+        self.resc1.add_victims(centroids[0][2])
+        self.resc2.add_victims(centroids[1][2])
+        self.resc3.add_victims(centroids[2][2])
+        self.resc4.add_victims(centroids[3][2])
+
+    def assign_map_to_rescuers(self):
+        """Atribui o mapa a cada resgatador"""
+        self.resc1.load_map(self.map)
+        self.resc2.load_map(self.map)
+        self.resc3.load_map(self.map)
+        self.resc4.load_map(self.map)
+        print(f"Maps received from the explorer")
+
+    def add_man_victims(self, victims):
+        for seq, data in victims.items():
+            self.victims[seq] = data
+
+    def go_rescuer(self):
+        self.resc1.go_save_victims()
+        self.resc2.go_save_victims()
+        self.resc3.go_save_victims()
+        self.resc4.go_save_victims()
+
+    def plan_to_rescuer(self):
+        for key, value in self.victims.items():
+            X_test = np.array([value[1][3], value[1][4], value[1][5]]).reshape(1, -1)
+            predicted_class = int(self.model.predict(X_test)[0])
+
+            value[1].append(predicted_class+1)
+
+            self.victims[key] = (value[0], value[1])
+
+
+        clusters = k_means_pp(self.victims)
+        save_clusters(clusters)
+        save_plot(clusters)
+
+        self.assign_victims_to_rescuers(clusters)
+        self.assign_map_to_rescuers()
+        # print(f"map:")
+        # self.map.draw()
+        self.go_rescuer()
+
